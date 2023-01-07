d99995a4f0c865bb22d8cd0433f90f00c94efa5f
khaphan-github <phanhoangkha01@gmail.com>
2023-01-07 23:49:50 +0700

    fix bug deploy

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -6,7 +6,7 @@ const MONGODB_USERNAME = process.env.MONGODB_USERNAME || '';
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || '';
 const MONGODB_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@clustersingapore.b43lfcm.mongodb.net/webrtc`;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
+const SERVER_PORT = process.env.SERVER_PORT ? process.env.SERVER_PORT : 3000;
 
 const API_PATH = process.env.API_PATH ?  process.env.API_PATH : '/api/v1';
 
