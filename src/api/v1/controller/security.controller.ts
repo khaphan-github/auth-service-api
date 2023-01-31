dbb79cdb7a5bcc487a8a8f0f7a0487178b4343e4
khaphan-github <phanhoangkha01@gmail.com>
2023-01-31 13:07:13 +0700

    format code

--- src/api/v1/controller/security.controller.ts
+++ src/api/v1/controller/security.controller.ts
@@ -4,12 +4,23 @@ import { handleAppClientAuthenticate, hanldeUserAuthenticate } from '../services
 import { ClientAccount } from '../payload/request/clientaccount.req';
 import { RefreshTokenReq } from '../payload/request/refreshToken.req';
 import { JWT } from '../services/jwt/jwt.service';
+import { ResponseBase, ResponseStatus } from '../payload/Res/response.payload';
 
 export class SecurityController {
     static appClientAuthenticate = (
         req: Request,
         res: Response,
         next: NextFunction) => {
+
+        const isNotNullRequest: boolean = req.body.clientId && req.body.clientSecret;
+
+        if (!isNotNullRequest) {
+            const _response =
+                ResponseBase(
+                    ResponseStatus.WRONG_FORMAT,
+                    'Request require cliendId and clientSecret but you missing');
+            return res.status(422).json(_response);
+        }
 
         const _client: ClientKey = {
             clientId: req.body.clientId,
